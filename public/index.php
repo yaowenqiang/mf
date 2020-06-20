ee98d03342e2cf6569e1f54a3252c97b482c017f
jacky.yao <jacky.yao@corp.to8to.com>
2020-06-20 23:09:54 +0800

    add logger demo

--- public/index.php
+++ public/index.php
@@ -7,12 +7,15 @@
  * Time: 23:20
  */
 
+use App\Kernel;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 
-require  __DIR__ . '../vendor/autoload.php';
+$loader = require  __DIR__ . '../vendor/autoload.php';
 
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 $kernel = new Kernel('dev', true);
 $request = Request::createFromGlobals();
--- src/Controller/MicroController.php
+++ src/Controller/MicroController.php
@@ -12,15 +12,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class MicroController extends AbstractController
 {
     /**
      * @Route("/random/{limit}")
      */
-    public function randomNumber($limit)
+    public function randomNumber(LoggerInterface $logger)
     {
-
+        $logger->info('this is an info log');
+        $logger->error('this is an error log');
+        $logger->critical('this is a critical log', [
+            'cause' => 'in_hurry'
+        ]);
+        $limit = 100;
         $number = random_int(0, $limit);
         return $this->render('micro/random.html.twig', [
             'number' => $number
